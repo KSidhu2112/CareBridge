d23e5bb9b428f53403603c40f264b4321c08d3ba
kurvasidhu <kurvasidhu2112@gmail.com>
2026-05-13 15:27:54 +0530

    Fix CORS policy to allow specific frontend domains

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,26 @@ import statsRouter from './routes/StatsRoute.js';
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+const allowedOrigins = [
+  "https://carebridgeadminsidhu.netlify.app",
+  "https://carebridgefrontend.netlify.app",
+  "https://carebridgedelivery.netlify.app",
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "http://localhost:5175"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+}));
 app.use(express.json());
 
 connectDB();
